303003b666e6d69a5828439ba401192ee9351635
Tom <twdickman@gmail.com>
2018-06-30 17:45:51 +1000

    Initial Commit

---
+++ porfolio_app.jsx
@@ -0,0 +1,19 @@
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      
+    }
+  }
+
+  render(){
+    return(
+      <NavBar />
+    )
+  }
+}
+
+
+class NavBar extends React.Component {
+  con
+}
